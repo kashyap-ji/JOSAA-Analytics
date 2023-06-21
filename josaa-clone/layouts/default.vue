e6b06c4adf7c49b4d7000127a8ae989189172c14
Sayan Dey Sarkar <sayandeysarkar2003@gmail.com>
2023-06-22 03:21:41 +0530

    updates

---
+++ josaa-clone/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <nav class="p-6 flex items-center justify-between bg-teal-800">
+            <NuxtLink to="/" class="text-xl text-white">JOSAA Analysis</NuxtLink>
+
+            <div class="flex items-center space-x-4">
+                <NuxtLink to="/" class="text-white hover:text-teal-300">Home</NuxtLink>
+                <NuxtLink to="/settings" class="text-white hover:text-teal-300">View</NuxtLink>
+            </div>
+        </nav>
+
+        <slot />
+
+        <footer class="p-6 flex flex-wrap items-center justify-between bg-gray-900">
+            <p class="text-gray-300">Copiright (c) Sayan 😂 2023</p>
+
+            <div class="flex mt-6 md:mt-0 items-center space-x-4">
+                    <NuxtLink to="/dashboard" class="py-4 px-6 bg-teal-900 hover:bg-teal-700 text-white rounded-xl">Dashboard</NuxtLink>
+                    <NuxtLink to="/insights" class="py-4 px-6 bg-teal-600 hover:bg-teal-700 text-white rounded-xl">Make Insights</NuxtLink>
+                    <NuxtLink to="/login" class="py-4 px-6 bg-teal-900 hover:bg-teal-700 text-white rounded-xl">Log in</NuxtLink>
+                    <NuxtLink to="/signup" class="py-4 px-6 bg-teal-600 hover:bg-teal-700 text-white rounded-xl">Sign up</NuxtLink>
+            </div>
+        </footer>
+    </div>
+</template>
---
+++ josaa-clone/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <hi class="h-screen max-w-full flex flex-col items-center justify-center">
+        Hello Everyone
+    </hi>
+</template>
